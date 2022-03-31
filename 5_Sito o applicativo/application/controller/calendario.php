4c4019036ae636abace3921e5e85d3d1cdeb257b
GioeleZanetti <59683307+GioeleZanetti@users.noreply.github.com>
2022-03-31 10:17:01 +0200

    Funzione index

--- 5_Sito o applicativo/application/controller/calendario.php
+++ 5_Sito o applicativo/application/controller/calendario.php
@@ -2,14 +2,20 @@
 
     class Calendario extends Controller{
 
-    public function impostaNegozio(){
-        $_SESSION['negozio_id'] = $_POST['negozio'];
-        parent::getModel('orario_model.php');
-        $model = new OrarioModel();
-        $this->view->render("calendario/index.php", false, array('dipendenti' => $model->ottieniDipendentiDiDatore()));
-        $inizio = new DateTime('2000-01-01');
-        $fine = new DateTime('2030-01-01');
-    }
+        public function index(){
+            parent::getModel('negozio_model.php');
+            $model = new NegozioModel();
+            $this->view->render('calendario/negozio.php', false, array('negozi' => $model->ottieniNegozi()));
+        }
+
+        public function impostaNegozio(){
+            $_SESSION['negozio_id'] = $_POST['negozio'];
+            parent::getModel('orario_model.php');
+            $model = new OrarioModel();
+            $this->view->render("calendario/index.php", false, array('dipendenti' => $model->ottieniDipendentiDiDatore()));
+            $inizio = new DateTime('2000-01-01');
+            $fine = new DateTime('2030-01-01');
+        }
 
         public function ottieniEventi(){
             parent::getModel('orario_model.php');
@@ -21,15 +27,15 @@
             return $json;
         }
 
-    public function salva(){
-        $data = json_decode($_POST['data'], true);
-        
-        $range = json_decode($data['range'], true);
-        $events = json_decode($data['events'], true);
-        parent::getModel('orario_model.php');
-        $model = new OrarioModel();
-        echo json_encode(array("status" => $model->salva($range, $events)));
-    }
+        public function salva(){
+            $data = json_decode($_POST['data'], true);
+            
+            $range = json_decode($data['range'], true);
+            $events = json_decode($data['events'], true);
+            parent::getModel('orario_model.php');
+            $model = new OrarioModel();
+            echo json_encode(array("status" => $model->salva($range, $events)));
+        }
 
     }
 
